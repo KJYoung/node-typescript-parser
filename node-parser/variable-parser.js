0077edec6cf8c69758df42e762733128d89b1c1d
KJYoung <jykim157@snu.ac.kr>
2022-12-09 00:13:46 +0900

    v0.0.1 More basic rules, preparation for operations

--- node-parser/variable-parser.js
+++ node-parser/variable-parser.js
@@ -5,6 +5,34 @@ const typescript_1 = require("typescript");
 const VariableDeclaration_1 = require("../declarations/VariableDeclaration");
 const TypescriptHeroGuards_1 = require("../type-guards/TypescriptHeroGuards");
 const parse_utilities_1 = require("./parse-utilities");
+function getExpressionType(left) {
+    // Consider operation...
+    // +, -, *, / , %, ++, --
+    // >, <, >=, <=, ==, !=
+    // &&, ||, !
+    // &, |, ^, ~, <<, >>, >>>
+    // =, +=, -=, *=, /=
+    // unary -, string concat +, conditional 'A ? B : C', typeof, instanceof
+    if (left && left.kind) {
+        switch (left.kind) {
+            case 8:
+            case 9: // NumericLiteral, BigIntLiteral
+                return "number";
+            case 10: // StringLiteral
+                return "string";
+            case 91:
+            case 106: // IfKeyword(false), VoidKeyword(true)
+                return "boolean";
+            case 209:
+                return getExpressionType(left.left);
+            default:
+                return `Complicated Expression ${left.kind}`;
+        }
+    }
+    else {
+        return `Complicated Expression`;
+    }
+}
 /**
  * Parse a variable. Information such as "is the variable const" are calculated here.
  *
@@ -16,8 +44,11 @@ function parseVariable(parent, node) {
     const isConst = node.declarationList.getChildren().some(o => o.kind === typescript_1.SyntaxKind.ConstKeyword);
     if (node.declarationList && node.declarationList.declarations) {
         node.declarationList.declarations.forEach((o) => {
-            var _a, _b;
-            let declaration;
+            var _a, _b, _c;
+            console.log(o);
+            let declaration; // = new VariableDeclaration(o.name.getText(), isConst, isNodeExported(node), ${the type string!}, node.getStart(), node.getEnd());
+            // Rule 1 : initializer.kind == Basic Literal?
+            // Rule 2 : initializer has operatorToken?
             switch ((_a = o.initializer) === null || _a === void 0 ? void 0 : _a.kind) {
                 case 8:
                 case 9: // NumericLiteral, BigIntLiteral
@@ -26,8 +57,20 @@ function parseVariable(parent, node) {
                 case 10: // StringLiteral
                     declaration = new VariableDeclaration_1.VariableDeclaration(o.name.getText(), isConst, parse_utilities_1.isNodeExported(node), "string", node.getStart(), node.getEnd());
                     break;
+                case 91:
+                case 106: // IfKeyword(false), VoidKeyword(true)
+                    declaration = new VariableDeclaration_1.VariableDeclaration(o.name.getText(), isConst, parse_utilities_1.isNodeExported(node), "boolean", node.getStart(), node.getEnd());
+                    break;
+                case 209: // ClassExpression, Follow left operand's type
+                    const initializer = o.initializer;
+                    if (initializer.left && initializer.operatorToken) {
+                        declaration = new VariableDeclaration_1.VariableDeclaration(o.name.getText(), isConst, parse_utilities_1.isNodeExported(node), getExpressionType(initializer.left), node.getStart(), node.getEnd());
+                    }
+                    else {
+                        declaration = new VariableDeclaration_1.VariableDeclaration(o.name.getText(), isConst, parse_utilities_1.isNodeExported(node), parse_utilities_1.getNodeType(o.type ? o.type : (_b = o.initializer) === null || _b === void 0 ? void 0 : _b.type), node.getStart(), node.getEnd());
+                    }
+                    break;
                 case 202: // PrefixUnaryExpression
-                    // console.log("CASE 202");
                     const parameters = o.initializer.parameters;
                     let nodeType = "";
                     parameters.forEach((param) => {
@@ -35,11 +78,11 @@ function parseVariable(parent, node) {
                     });
                     // nodeType += getNodeType((o.initializer as unknown as {body : any}).body.type )
                     nodeType += " => unknown";
-                    // console.log(nodeType);
                     declaration = new VariableDeclaration_1.VariableDeclaration(o.name.getText(), isConst, parse_utilities_1.isNodeExported(node), nodeType, node.getStart(), node.getEnd());
                     break;
                 default:
-                    declaration = new VariableDeclaration_1.VariableDeclaration(o.name.getText(), isConst, parse_utilities_1.isNodeExported(node), parse_utilities_1.getNodeType(o.type ? o.type : (_b = o.initializer) === null || _b === void 0 ? void 0 : _b.type), node.getStart(), node.getEnd());
+                    // console.log(`o.initializer.kind : ${o.initializer?.kind} | ${(o.name as unknown as {escapedText : string}).escapedText}`);
+                    declaration = new VariableDeclaration_1.VariableDeclaration(o.name.getText(), isConst, parse_utilities_1.isNodeExported(node), parse_utilities_1.getNodeType(o.type ? o.type : (_c = o.initializer) === null || _c === void 0 ? void 0 : _c.type), node.getStart(), node.getEnd());
                     break;
             }
             if (TypescriptHeroGuards_1.isCallableDeclaration(parent)) {
--- src/node-parser/variable-parser.ts
+++ src/node-parser/variable-parser.ts
@@ -6,6 +6,32 @@ import { Resource } from '../resources/Resource';
 import { isCallableDeclaration } from '../type-guards/TypescriptHeroGuards';
 import { getNodeType, isNodeExported } from './parse-utilities';
 
+function getExpressionType(left: any): string {
+    // Consider operation...
+    // +, -, *, / , %, ++, --
+    // >, <, >=, <=, ==, !=
+    // &&, ||, !
+    // &, |, ^, ~, <<, >>, >>>
+    // =, +=, -=, *=, /=
+    // unary -, string concat +, conditional 'A ? B : C', typeof, instanceof
+    if(left && left.kind ){
+        switch(left.kind){
+            case 8: case 9: // NumericLiteral, BigIntLiteral
+                return "number";
+            case 10: // StringLiteral
+                return "string";
+            case 91: case 106: // IfKeyword(false), VoidKeyword(true)
+                return "boolean";
+            case 209:
+                return getExpressionType(left.left);
+            default:
+                return `Complicated Expression ${left.kind}`;
+        }
+    }else{
+        return `Complicated Expression`;
+    }
+}
+
 /**
  * Parse a variable. Information such as "is the variable const" are calculated here.
  *
@@ -17,7 +43,11 @@ export function parseVariable(parent: Resource | CallableDeclaration, node: Vari
     const isConst = node.declarationList.getChildren().some(o => o.kind === SyntaxKind.ConstKeyword);
     if (node.declarationList && node.declarationList.declarations) {
         node.declarationList.declarations.forEach((o) => {
-            let declaration;
+            console.log(o);
+            let declaration; // = new VariableDeclaration(o.name.getText(), isConst, isNodeExported(node), ${the type string!}, node.getStart(), node.getEnd());
+
+            // Rule 1 : initializer.kind == Basic Literal?
+            // Rule 2 : initializer has operatorToken?
             switch(o.initializer?.kind) {
                 case 8: case 9: // NumericLiteral, BigIntLiteral
                     declaration = new VariableDeclaration(o.name.getText(), isConst, isNodeExported(node), "number", node.getStart(), node.getEnd());
@@ -25,8 +55,18 @@ export function parseVariable(parent: Resource | CallableDeclaration, node: Vari
                 case 10: // StringLiteral
                     declaration = new VariableDeclaration(o.name.getText(), isConst, isNodeExported(node), "string", node.getStart(), node.getEnd());
                     break;
+                case 91: case 106: // IfKeyword(false), VoidKeyword(true)
+                    declaration = new VariableDeclaration(o.name.getText(), isConst, isNodeExported(node), "boolean", node.getStart(), node.getEnd());
+                    break;
+                case 209: // ClassExpression, Follow left operand's type
+                    const initializer : {left : any, operatorToken : any } = (o.initializer as unknown as  {left : any, operatorToken : any });  
+                    if(initializer.left && initializer.operatorToken){
+                        declaration = new VariableDeclaration(o.name.getText(), isConst, isNodeExported(node), getExpressionType(initializer.left), node.getStart(), node.getEnd());
+                    }else{
+                        declaration = new VariableDeclaration(o.name.getText(), isConst, isNodeExported(node), getNodeType(o.type ? o.type : (o.initializer as unknown as { type : any })?.type), node.getStart(), node.getEnd());
+                    }
+                    break;
                 case 202: // PrefixUnaryExpression
-                    // console.log("CASE 202");
                     const parameters = (o.initializer as unknown as {parameters : any}).parameters;
                     let nodeType = "";
                     parameters.forEach((param : any) => {
@@ -34,10 +74,10 @@ export function parseVariable(parent: Resource | CallableDeclaration, node: Vari
                     });
                     // nodeType += getNodeType((o.initializer as unknown as {body : any}).body.type )
                     nodeType += " => unknown";
-                    // console.log(nodeType);
                     declaration = new VariableDeclaration(o.name.getText(), isConst, isNodeExported(node), nodeType, node.getStart(), node.getEnd());
                     break;
                 default:
+                    // console.log(`o.initializer.kind : ${o.initializer?.kind} | ${(o.name as unknown as {escapedText : string}).escapedText}`);
                     declaration = new VariableDeclaration(o.name.getText(), isConst, isNodeExported(node), getNodeType(o.type ? o.type : (o.initializer as unknown as { type : any })?.type), node.getStart(), node.getEnd());
                     break;
             }
